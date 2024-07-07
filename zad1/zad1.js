bb282db64278833c1fc7802176b4c10a5719f1e4
fnowak <filip.nowak7@gmail.com>
2024-07-07 16:04:32 +0200

    zad1

--- zad1/zad1.js
+++ zad1/zad1.js
@@ -1,12 +1,11 @@
 let cities = ['Warsaw', 'Berlin', 'Paris', 'London', 'New York', 'Tokio', 'Moscow'];
-
+const doc = document.body;
 let ulElement = document.createElement('ul');
 cities.forEach(function (element){
     let newLi = document.createElement('li');
     newLi.setAttribute('class', 'city')
     newLi.textContent = element
-    ulElement.append(newLi)
+    ulElement.appendChild(newLi)
 });
-let attrNode = document.createAttribute('class');
-
-console.log(ulElement)
+doc.append(ulElement);
+console.log(doc)
